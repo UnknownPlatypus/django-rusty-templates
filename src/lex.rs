6f4400aca371c2d126eb1dde6f5fed6b01e0dc15
Lily Foote <code@lilyf.org>
2024-10-06 13:21:16 +0100

    Check validity of variable and filter names
    Standardise checking for invalid remainders.

--- src/lex.rs
+++ src/lex.rs
@@ -1,4 +1,5 @@
 use thiserror::Error;
+use unicode_xid::UnicodeXID;
 
 const START_TAG_LEN: usize = 2;
 const END_TAG_LEN: usize = 2;
@@ -219,6 +220,10 @@ pub enum VariableLexerError {
     MissingTranslatedString { at: (usize, usize) },
     #[error("Could not parse the remainder")]
     InvalidRemainder { at: (usize, usize) },
+    #[error("Expected a valid filter name")]
+    InvalidFilterName { at: (usize, usize) },
+    #[error("Expected a valid variable name")]
+    InvalidVariableName { at: (usize, usize) },
 }
 
 pub struct VariableLexer<'t> {
@@ -234,32 +239,6 @@ impl<'t> VariableLexer<'t> {
             rest: rest.trim_end(),
             byte: start + START_TAG_LEN + variable.len() - rest.len(),
             mode: Mode::Variable,
-        }
-    }
-
-    fn lex_to_end(&mut self, token_type: VariableTokenType) -> VariableToken<'t> {
-        let start = self.byte;
-        let content = self.rest;
-        self.byte += content.len();
-        self.rest = "";
-        let at = (start, self.byte);
-        VariableToken {
-            token_type,
-            content,
-            at,
-        }
-    }
-
-    fn lex_to_next(&mut self, next: usize, token_type: VariableTokenType) -> VariableToken<'t> {
-        let start = self.byte;
-        let content = &self.rest[..next];
-        self.byte += content.len() + 1;
-        self.rest = &self.rest[next + 1..];
-        let at = (start, self.byte - 1);
-        VariableToken {
-            token_type,
-            content,
-            at,
         }
     }
 
@@ -355,11 +334,137 @@ impl<'t> VariableLexer<'t> {
         }
     }
 
-    fn lex_variable(&mut self) -> VariableToken<'t> {
-        let next_filter = self.rest.find("|");
-        match next_filter {
-            None => self.lex_to_end(VariableTokenType::Variable),
-            Some(n) => self.lex_to_next(n, VariableTokenType::Variable),
+    fn lex_variable(&mut self) -> Result<VariableToken<'t>, VariableLexerError> {
+        let end = self
+            .rest
+            .find(|c: char| !c.is_xid_continue() && c != '.')
+            .unwrap_or(self.rest.len());
+        let content = &self.rest[..end];
+
+        let mut offset = 0;
+        for var in content.split('.') {
+            match var.chars().next() {
+                Some(c) if c.is_xid_start() && c != '_' => {
+                    offset += var.len() + 1;
+                    continue;
+                }
+                _ => {
+                    let at = (self.byte + offset, self.byte + offset + var.len());
+                    self.rest = "";
+                    return Err(VariableLexerError::InvalidVariableName { at });
+                }
+            }
+        }
+        let at = (self.byte, self.byte + end);
+        self.byte += end;
+        self.rest = &self.rest[end..];
+        Ok(VariableToken {
+            token_type: VariableTokenType::Variable,
+            content,
+            at,
+        })
+    }
+
+    fn lex_filter(&mut self) -> Result<VariableToken<'t>, VariableLexerError> {
+        let filter = self.rest.trim_start();
+        let start = self.rest.len() - filter.len();
+        self.byte += start;
+        self.rest = &self.rest[start..];
+
+        let end = filter
+            .find(|c: char| !c.is_xid_continue())
+            .unwrap_or(filter.len());
+        let filter = &filter[..end];
+
+        match filter.chars().next() {
+            Some(c) if c.is_xid_start() => {
+                let at = (self.byte, self.byte + end);
+                self.byte += end;
+                self.rest = &self.rest[end..];
+                Ok(VariableToken {
+                    token_type: VariableTokenType::Filter,
+                    content: filter,
+                    at,
+                })
+            }
+            _ => {
+                let next = [self.rest.find("|"), self.rest.find(":")]
+                    .iter()
+                    .filter_map(|n| *n)
+                    .min()
+                    .unwrap_or(self.rest.len());
+                let at = (self.byte, self.byte + next);
+                self.rest = "";
+                Err(VariableLexerError::InvalidFilterName { at })
+            }
+        }
+    }
+
+    fn lex_argument(&mut self) -> Result<VariableToken<'t>, VariableLexerError> {
+        let mut chars = self.rest.chars();
+        match chars.next().unwrap() {
+            '_' => {
+                if let Some('(') = chars.next() {
+                    self.lex_translated(&mut chars)
+                } else {
+                    let start = self.byte;
+                    let end = self
+                        .rest
+                        .find(char::is_whitespace)
+                        .unwrap_or(self.rest.len());
+                    let at = (start, start + end);
+                    self.byte += self.rest.len();
+                    self.rest = "";
+                    Err(VariableLexerError::LeadingUnderscore { at })
+                }
+            }
+            '\'' => self.lex_text(&mut chars, '\''),
+            '"' => self.lex_text(&mut chars, '"'),
+            '0'..='9' => Ok(self.lex_numeric()),
+            _ => self.lex_variable(),
+        }
+    }
+
+    fn lex_remainder(
+        &mut self,
+        token: VariableToken<'t>,
+        remainder: &'t str,
+        start_next: usize,
+    ) -> Result<VariableToken<'t>, VariableLexerError> {
+        match remainder.find(|c: char| !c.is_whitespace()) {
+            None => {
+                self.rest = &self.rest[start_next..];
+                self.byte += start_next;
+                Ok(token)
+            }
+            Some(n) => {
+                let at = (self.byte + n, self.byte + remainder.trim_end().len());
+                self.rest = "";
+                Err(VariableLexerError::InvalidRemainder { at })
+            }
+        }
+    }
+
+    fn remainder_to_filter(&mut self) -> (&'t str, usize) {
+        self.mode = Mode::Filter;
+        match self.rest.find("|") {
+            None => (self.rest, self.rest.len()),
+            Some(f) => (&self.rest[..f], f + 1),
+        }
+    }
+
+    fn remainder_to_filter_or_argument(&mut self) -> (&'t str, usize) {
+        match (self.rest.find("|"), self.rest.find(":")) {
+            (None, None) => (self.rest, self.rest.len()),
+            (None, Some(a)) => {
+                self.mode = Mode::Argument;
+                (&self.rest[..a], a + 1)
+            }
+            (Some(f), Some(a)) if a < f => {
+                self.mode = Mode::Argument;
+                (&self.rest[..a], a + 1)
+            }
+            (Some(f), _) => (&self.rest[..f], f + 1),
         }
     }
 }
@@ -371,78 +476,20 @@ impl<'t> Iterator for VariableLexer<'t> {
         if self.rest.is_empty() {
             return None;
         }
-        Some(match self.mode {
-            Mode::Variable => {
-                self.mode = Mode::Filter;
-                Ok(self.lex_variable())
-            }
-            Mode::Filter => {
-                let next_filter = self.rest.find("|");
-                let next_argument = self.rest.find(":");
-                match (next_filter, next_argument) {
-                    (None, None) => Ok(self.lex_to_end(VariableTokenType::Filter)),
-                    (None, Some(n)) => {
-                        self.mode = Mode::Argument;
-                        Ok(self.lex_to_next(n, VariableTokenType::Filter))
-                    }
-                    (Some(f), Some(a)) if a < f => {
-                        self.mode = Mode::Argument;
-                        Ok(self.lex_to_next(a, VariableTokenType::Filter))
-                    }
-                    (Some(n), _) => Ok(self.lex_to_next(n, VariableTokenType::Filter)),
-                }
-            }
-            Mode::Argument => {
-                self.mode = Mode::Filter;
-                let mut chars = self.rest.chars();
-                let token = match chars.next().unwrap() {
-                    '_' => {
-                        if let Some('(') = chars.next() {
-                            self.lex_translated(&mut chars)
-                        } else {
-                            let start = self.byte;
-                            let end = self
-                                .rest
-                                .find(char::is_whitespace)
-                                .unwrap_or(self.rest.len());
-                            let at = (start, start + end);
-                            self.byte += self.rest.len();
-                            self.rest = "";
-                            return Some(Err(VariableLexerError::LeadingUnderscore { at }));
-                        }
-                    }
-                    '\'' => self.lex_text(&mut chars, '\''),
-                    '"' => self.lex_text(&mut chars, '"'),
-                    '0'..='9' => Ok(self.lex_numeric()),
-                    _ => return Some(Ok(self.lex_variable())),
-                };
-                match self.rest.find("|") {
-                    Some(n) => {
-                        let remainder = &self.rest[..n];
-                        if remainder.trim().is_empty() {
-                            self.rest = &self.rest[n + 1..];
-                            self.byte += n + 1;
-                            token
-                        } else {
-                            let start = self.byte;
-                            let at = (start, self.byte + remainder.len());
-                            self.rest = "";
-                            Err(VariableLexerError::InvalidRemainder { at })
-                        }
-                    }
-                    None => {
-                        if self.rest.trim().is_empty() {
-                            token
-                        } else {
-                            let start = self.byte;
-                            let at = (start, self.byte + self.rest.len());
-                            self.rest = "";
-                            Err(VariableLexerError::InvalidRemainder { at })
-                        }
-                    }
-                }
-            }
-        })
+        let token = match self.mode {
+            Mode::Variable => self.lex_variable(),
+            Mode::Filter => self.lex_filter(),
+            Mode::Argument => self.lex_argument(),
+        };
+        let token = match token {
+            Err(e) => return Some(Err(e)),
+            Ok(token) => token,
+        };
+        let (remainder, start_next) = match self.mode {
+            Mode::Filter => self.remainder_to_filter_or_argument(),
+            Mode::Variable | Mode::Argument => self.remainder_to_filter(),
+        };
+        Some(self.lex_remainder(token, remainder, start_next))
     }
 }
 
@@ -788,6 +835,28 @@ mod variable_lexer_tests {
     }
 
     #[test]
+    fn test_lex_variable_start_underscore() {
+        let variable = " _foo.bar ";
+        let lexer = VariableLexer::new(variable, 0);
+        let tokens: Vec<_> = lexer.collect();
+        assert_eq!(
+            tokens,
+            vec![Err(VariableLexerError::InvalidVariableName { at: (3, 7) })]
+        );
+    }
+
+    #[test]
+    fn test_lex_attribute_start_underscore() {
+        let variable = " foo._bar ";
+        let lexer = VariableLexer::new(variable, 0);
+        let tokens: Vec<_> = lexer.collect();
+        assert_eq!(
+            tokens,
+            vec![Err(VariableLexerError::InvalidVariableName { at: (7, 11) })]
+        );
+    }
+
+    #[test]
     fn test_lex_filter() {
         let variable = " foo.bar|title ";
         let lexer = VariableLexer::new(variable, 0);
@@ -805,6 +874,42 @@ mod variable_lexer_tests {
                     content: "title",
                     at: (11, 16),
                 }),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_lex_filter_remainder() {
+        let variable = " foo.bar|title'foo' ";
+        let lexer = VariableLexer::new(variable, 0);
+        let tokens: Vec<_> = lexer.collect();
+        assert_eq!(
+            tokens,
+            vec![
+                Ok(VariableToken {
+                    token_type: VariableTokenType::Variable,
+                    content: "foo.bar",
+                    at: (3, 10),
+                }),
+                Err(VariableLexerError::InvalidRemainder { at: (16, 21) }),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_lex_filter_invalid_start() {
+        let variable = " foo.bar|'foo' ";
+        let lexer = VariableLexer::new(variable, 0);
+        let tokens: Vec<_> = lexer.collect();
+        assert_eq!(
+            tokens,
+            vec![
+                Ok(VariableToken {
+                    token_type: VariableTokenType::Variable,
+                    content: "foo.bar",
+                    at: (3, 10),
+                }),
+                Err(VariableLexerError::InvalidFilterName { at: (11, 16) }),
             ]
         );
     }
