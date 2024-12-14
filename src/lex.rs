687ad607a4fbed3c9ab895af57cedb6fb95a3647
Lily Foote <code@lilyf.org>
2024-12-14 21:33:44 +0000

    Add lex_tag function and TagLexerError

--- src/lex.rs
+++ src/lex.rs
@@ -1,7 +1,9 @@
 mod core;
+mod tag;
 mod variable;
 
 pub use core::{Lexer, TokenType};
+pub use tag::{lex_tag, TagLexerError, TagParts};
 pub use variable::{lex_variable, Argument, ArgumentType, VariableLexerError};
 
 pub const START_TAG_LEN: usize = 2;
