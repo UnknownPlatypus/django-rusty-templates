e8ccad548188a17f672292646513ec6d1c1607f1
Lily Foote <code@lilyf.org>
2024-12-22 22:35:47 +0000

    Mark test views as known uncovered
    These are (currently) only here for testing the `{% url %}` template
    tag, so their implementation is irrelevant.

--- tests/views.py
+++ tests/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "templates/home.txt")
+    return render(request, "templates/home.txt")  # pragma: no cover
 
 
 def bio(request, username):
-    return render(request, "templates/bio.txt", {"username": username})
+    return render(
+        request, "templates/bio.txt", {"username": username}
+    )  # pragma: no cover
