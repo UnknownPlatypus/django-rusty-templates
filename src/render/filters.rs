d480f680f826bcf42846afeedcfb2b4cc39eacc1
Jonathan Sundqvist <jonathan+git@argpar.se>
2025-03-01 17:07:49 +0100

    The default value when context empty is Content::Int

--- src/render/filters.rs
+++ src/render/filters.rs
@@ -402,9 +402,8 @@ mod tests {
             assert_eq!(result, "a-b");
 
             let engine = EngineData::empty();
-            let template_string = "{{ var|slugify }}".to_string();
+            let template_string = "{{ var|default:1|slugify }}".to_string();
             let context = PyDict::new(py);
-            context.set_item("var", 1).unwrap();
             let template = Template::new_from_string(py, template_string, &engine).unwrap();
             let result = template.render(py, Some(context), None).unwrap();
 
