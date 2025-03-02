9d6967e288c0623a0c30e7f13ec3d971cd72e064
Lily Foote <code@lilyf.org>
2025-03-02 13:24:50 +0000

    Add tests for invalid keyword usage in if tags

--- tests/tags/test_if.py
+++ tests/tags/test_if.py
@@ -1,4 +1,6 @@
+import pytest
 from django.template import engines
+from django.template.exceptions import TemplateSyntaxError
 
 
 def test_render_if_true():
@@ -18,3 +20,115 @@ def test_render_if_false():
 
     assert django_template.render({}) == ""
     assert rust_template.render({}) == ""
+
+
+def test_invalid_and_position():
+    template = "{% if and %}{{ foo }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    assert str(exc_info.value) == "Not expecting 'and' in this position in if tag."
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    print(exc_info.value)
+    assert str(exc_info.value) == """\
+"""
+
+
+def test_invalid_or_position():
+    template = "{% if or %}{{ foo }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    assert str(exc_info.value) == "Not expecting 'or' in this position in if tag."
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    print(exc_info.value)
+    assert str(exc_info.value) == """\
+"""
+
+
+def test_unexpected_end_of_expression():
+    template = "{% if not %}{{ foo }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    assert str(exc_info.value) == "Unexpected end of expression in if tag."
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    print(exc_info.value)
+    assert str(exc_info.value) == """\
+"""
+
+
+def test_invalid_in_position():
+    template = "{% if in %}{{ foo }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    assert str(exc_info.value) == "Not expecting 'in' in this position in if tag."
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    print(exc_info.value)
+    assert str(exc_info.value) == """\
+"""
+
+
+def test_invalid_not_in_position():
+    template = "{% if not in %}{{ foo }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    assert str(exc_info.value) == "Not expecting 'not in' in this position in if tag."
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    print(exc_info.value)
+    assert str(exc_info.value) == """\
+"""
+
+
+def test_invalid_is_position():
+    template = "{% if is %}{{ foo }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    assert str(exc_info.value) == "Not expecting 'is' in this position in if tag."
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    print(exc_info.value)
+    assert str(exc_info.value) == """\
+"""
+
+
+def test_invalid_is_not_position():
+    template = "{% if is not %}{{ foo }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    assert str(exc_info.value) == "Not expecting 'is not' in this position in if tag."
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    print(exc_info.value)
+    assert str(exc_info.value) == """\
+"""
