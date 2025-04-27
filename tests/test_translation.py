7e272e91a21d0401d04a505c611fe385f3035adb
Lily Foote <code@lilyf.org>
2025-04-27 10:50:21 +0100

    Test filter argument translation without autoescape

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -4,14 +4,14 @@ from .utils import render
 
 
 def test_translate_default():
-    expected = "Welcome\n"
+    expected = "Welcome\n\nGoodbye\n\n"
 
     assert render("translation.txt", {}, using="django") == expected
     assert render("translation.txt", {}, using="rusty") == expected
 
 
 def test_translate_missing():
-    expected = "Welcome\n"
+    expected = "Welcome\n\nGoodbye\n\n"
 
     with override("fr"):  # Deliberately missing translation
         assert render("translation.txt", {}, using="django") == expected
@@ -19,7 +19,7 @@ def test_translate_missing():
 
 
 def test_translate_valid():
-    expected = "Willkommen\n"
+    expected = "Willkommen\n\nAuf Wiedersehen\n\n"
     with override("de"):
         assert render("translation.txt", {}, using="django") == expected
         assert render("translation.txt", {}, using="rusty") == expected
