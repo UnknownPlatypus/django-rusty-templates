d361a8ea74846e57d73398d85a8575b8f04bb0a7
Lily Acorn <code@lilyf.org>
2025-09-22 11:31:20 +0100

    Use Cow<'static, str> instead of &'static str for end tag names
    This will allow using dynamic end tag names (i.e. for custom tags)
    without needing to allocate a `String` for a known-at-compile-time end
    tag name.

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,4 @@
+use std::borrow::Cow;
 use std::collections::{HashMap, HashSet};
 use std::iter::Peekable;
 use std::sync::Arc;
@@ -483,8 +484,8 @@ enum EndTagType {
 }
 
 impl EndTagType {
-    fn as_str(&self) -> &'static str {
-        match self {
+    fn as_cow(&self) -> Cow<'static, str> {
+        let end_tag = match self {
             Self::Autoescape => "endautoescape",
             Self::Elif => "elif",
             Self::Else => "else",
@@ -492,7 +493,8 @@ impl EndTagType {
             Self::Empty => "empty",
             Self::EndFor => "endfor",
             Self::Verbatim => "endverbatim",
-        }
+        };
+        Cow::Borrowed(end_tag)
     }
 }
 
@@ -504,8 +506,8 @@ struct EndTag {
 }
 
 impl EndTag {
-    fn as_str(&self) -> &'static str {
-        self.end.as_str()
+    fn as_cow(&self) -> Cow<'static, str> {
+        self.end.as_cow()
     }
 }
 
@@ -645,7 +647,7 @@ pub enum ParseError {
     },
     #[error("Unclosed '{start}' tag. Looking for one of: {expected}")]
     MissingEndTag {
-        start: &'static str,
+        start: Cow<'static, str>,
         expected: String,
         #[label("started here")]
         at: SourceSpan,
@@ -731,7 +733,7 @@ pub enum ParseError {
     },
     #[error("Unexpected tag {unexpected}")]
     UnexpectedEndTag {
-        unexpected: &'static str,
+        unexpected: Cow<'static, str>,
         #[label("unexpected tag")]
         at: SourceSpan,
     },
@@ -748,7 +750,7 @@ pub enum ParseError {
     },
     #[error("Unexpected tag {unexpected}, expected {expected}")]
     WrongEndTag {
-        unexpected: &'static str,
+        unexpected: Cow<'static, str>,
         expected: String,
         #[label("unexpected tag")]
         at: SourceSpan,
@@ -887,7 +889,7 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
                     Either::Right(end_tag) => {
                         return Err(ParseError::UnexpectedEndTag {
                             at: end_tag.at.into(),
-                            unexpected: end_tag.as_str(),
+                            unexpected: end_tag.as_cow(),
                         }
                         .into());
                     }
@@ -901,7 +903,7 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
     fn parse_until(
         &mut self,
         until: Vec<EndTagType>,
-        start: &'static str,
+        start: Cow<'static, str>,
         start_at: (usize, usize),
     ) -> Result<(Vec<TokenTree>, EndTag), PyParseError> {
         let mut nodes = Vec::new();
@@ -925,10 +927,10 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
                             return Err(ParseError::WrongEndTag {
                                 expected: until
                                     .iter()
-                                    .map(|u| u.as_str())
+                                    .map(|u| u.as_cow())
                                     .collect::<Vec<_>>()
                                     .join(", "),
-                                unexpected: end_tag.as_str(),
+                                unexpected: end_tag.as_cow(),
                                 at: end_tag.at.into(),
                                 start_at: start_at.into(),
                             }
@@ -943,7 +945,7 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
             start,
             expected: until
                 .iter()
-                .map(|u| u.as_str())
+                .map(|u| u.as_cow())
                 .collect::<Vec<_>>()
                 .join(", "),
             at: start_at.into(),
@@ -1406,7 +1408,7 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
         parts: TagParts,
     ) -> Result<TokenTree, PyParseError> {
         let token = lex_autoescape_argument(self.template, parts).map_err(ParseError::from)?;
-        let (nodes, _) = self.parse_until(vec![EndTagType::Autoescape], "autoescape", at)?;
+        let (nodes, _) = self.parse_until(vec![EndTagType::Autoescape], "autoescape".into(), at)?;
         Ok(TokenTree::Tag(Tag::Autoescape {
             enabled: token.enabled,
             nodes,
@@ -1422,7 +1424,7 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
         let condition = parse_if_condition(self, parts, at)?;
         let (nodes, end_tag) = self.parse_until(
             vec![EndTagType::Elif, EndTagType::Else, EndTagType::EndIf],
-            start,
+            start.into(),
             at,
         )?;
         let falsey = match end_tag {
@@ -1436,7 +1438,7 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
                 end: EndTagType::Else,
                 parts: _parts,
             } => {
-                let (nodes, _) = self.parse_until(vec![EndTagType::EndIf], "else", at)?;
+                let (nodes, _) = self.parse_until(vec![EndTagType::EndIf], "else".into(), at)?;
                 Some(nodes)
             }
             EndTag {
@@ -1460,8 +1462,11 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
     ) -> Result<TokenTree, PyParseError> {
         self.forloop_depth += 1;
         let (iterable, variables, reversed) = parse_for_loop(self, parts, at)?;
-        let (nodes, end_tag) =
-            self.parse_until(vec![EndTagType::Empty, EndTagType::EndFor], "for", at)?;
+        let (nodes, end_tag) = self.parse_until(
+            vec![EndTagType::Empty, EndTagType::EndFor],
+            "for".into(),
+            at,
+        )?;
         self.forloop_depth -= 1;
         let empty = match end_tag {
             EndTag {
@@ -1469,7 +1474,7 @@ impl<'t, 'l, 'py> Parser<'t, 'l, 'py> {
                 end: EndTagType::Empty,
                 parts: _parts,
             } => {
-                let (nodes, _) = self.parse_until(vec![EndTagType::EndFor], "empty", at)?;
+                let (nodes, _) = self.parse_until(vec![EndTagType::EndFor], "empty".into(), at)?;
                 Some(nodes)
             }
             EndTag {
