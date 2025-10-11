41e35bd8b12db359c075f9b71f5d621af9e075ee
Lily Acorn <code@lilyf.org>
2025-10-11 18:28:06 +0100

    Test syntax error during parse_until

--- tests/tags/test_if.py
+++ tests/tags/test_if.py
@@ -1555,3 +1555,26 @@ def test_if_invalid_variable():
    ╰────
 """
     assert str(exc_info.value) == expected
+
+
+def test_if_invalid_content():
+    template = "{% if a %}{{ a- }}{% endif %}"
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["django"].from_string(template)
+
+    expected = "Could not parse the remainder: '-' from 'a-'"
+    assert str(exc_info.value) == expected
+
+    with pytest.raises(TemplateSyntaxError) as exc_info:
+        engines["rusty"].from_string(template)
+
+    expected = """\
+  × Expected a valid variable name
+   ╭────
+ 1 │ {% if a %}{{ a- }}{% endif %}
+   ·              ─┬
+   ·               ╰── here
+   ╰────
+"""
+    assert str(exc_info.value) == expected
