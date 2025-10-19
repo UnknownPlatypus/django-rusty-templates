cadd51bc3b4e0c7296594c0cffd79a98fa5de7fa
Thibaut Decombe <thibaut.decombe@gmail.com>
2025-10-19 22:24:13 +0200

    Update test_lower.py

--- tests/filters/test_lower.py
+++ tests/filters/test_lower.py
@@ -1,28 +1,13 @@
-from django.template import engines
-
-
-def test_lower_integer():
+def test_lower_integer(assert_render):
     template = "{{ foo|default:3|lower }}"
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    assert django_template.render({}) == "3"
-    assert rust_template.render({}) == "3"
+    assert_render(template=template, context={}, expected="3")
 
 
-def test_lower_float():
+def test_lower_float(assert_render):
     template = "{{ foo|default:3.7|lower }}"
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    assert django_template.render({}) == "3.7"
-    assert rust_template.render({}) == "3.7"
+    assert_render(template=template, context={}, expected="3.7")
 
 
-def test_lower_bool():
+def test_lower_bool(assert_render):
     template = "{% for x in 'ab' %}{{ forloop.first|lower }}{% endfor %}"
-    django_template = engines["django"].from_string(template)
-    rust_template = engines["rusty"].from_string(template)
-
-    assert django_template.render({}) == "truefalse"
-    assert rust_template.render({}) == "truefalse"
+    assert_render(template=template, context={}, expected="truefalse")
