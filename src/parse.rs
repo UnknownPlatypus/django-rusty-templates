a01f7c7cda14a444fb8ab28c47f5745b86bb50fa
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-04-19 10:37:27 +0200

    Use extract_as_variable

--- src/parse.rs
+++ src/parse.rs
@@ -1979,20 +1979,7 @@ impl<'t, 'py> Parser<'t, 'py> {
     fn parse_querystring(&self, at: At, parts: TagParts) -> Result<TokenTree, ParseError> {
         let mut tokens: Vec<_> =
             TagElementKwargLexer::new(self.template, parts).collect::<Result<Vec<_>, _>>()?;
-
-        tokens
-            .iter()
-            .for_each(|token| println!("{:?} -> {}", token, self.template.content(token.at)));
-        let tokens_count = tokens.len();
-
-        let target_var =
-            if tokens_count >= 2 && self.template.content(tokens[tokens_count - 2].at) == "as" {
-                let last = tokens.pop().expect("tokens should be length 2 or more");
-                tokens.pop();
-                Some(self.template.content(last.at).to_string())
-            } else {
-                None
-            };
+        let target_var = extract_as_variable(&mut tokens, &self.template)?;
 
         let mut query_dict = None;
         let mut kwargs = vec![];
